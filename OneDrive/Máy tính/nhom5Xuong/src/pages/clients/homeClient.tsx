48088abc66e8a538b1db1b1fd43d7c136e9a51a8
luong0910 <luongphamhy89bk@gmail.com>
2025-03-16 22:03:51 +0700

    up lai

---
+++ OneDrive/Máy tính/nhom5Xuong/src/pages/clients/homeClient.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+import p1 from "./img/p1.png";
+import p2 from "./img/p2.png";
+import p3 from "./img/p3.jfif";
+import p4 from "./img/p4.png";
+import p5 from "./img/p5.png";
+import p6 from "./img/p6.jfif";
+import p7 from "./img/p7.jfif";
+import p8 from "./img/p8.png";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+
+<img
+  src="https://s3-alpha-sig.figma.com/img/98fb/219f/a11f805aade2224f1d6658764a2395df?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=aOD9PwxEVc1zbTWYYpjwtLOgG1KAwbMH3uLbPjgGDkoKBuRIf-UdSLvIoG26LOI-cimx~~HU8PnFhd7I6tvr9SV-frL-yiAN75ShE6XPaFG8n0b8fGEGK5HnfkYG8~cnGlFmd0QBaauHiqjEz1n6b3IPuIPuQ580uiPZaFsloDqfCOB~e0dfWf5fvAa1GT~kGwPmDessVzbbGxFKjNGPFiwipoyR39CBg45Y1TAwUu5XRqdCjKCyXtvZFUseQzGFinHB6vFF1wDBRDRHE-4oRyMndubLEpfN3YwBxjrEW6S16rLIcwyve9OmRPo94qhruDgyRuwJoL9JJKPjsY1dVQ__"
+  alt="Background"
+  className="w-full h-auto object-cover rounded-lg"
+/>
+
+<div className="absolute inset-0 bg-opacity-30 rounded-lg"></div>
+
+
+<div className="absolute inset-0 flex items-center justify-end pr-30 translate-y-150">
+  <div className="bg-[#FEF6EC] p-8 rounded-lg shadow-lg text-left w-[800px] h-[650px] ">
+    <p className="text-3xl text-gray-800 uppercase font-semibold">New Arrival</p>
+    <h1 className="text-7xl font-extrabold mt-5 text-[#B68D40] leading-tight">
+      Discover Our <br /> New Collection
+    </h1>
+    <p className="mt-15 text-gray-700 text-3xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.
+    </p>
+    <button className="mt-20 bg-[#B68D40] text-7xl text-white font-bold py-3 px-6 rounded-lg">
+      BUY NOW
+    </button>
+  </div>
+</div>
+
+      <div className="text-center py-12 bg-gray-100">
+      <h2 className="text-3xl font-extrabold text-gray-800">Browse The Range.</h2>
+      <p className="text-gray-600 mt-2">Discover our beautifully curated collections.</p>
+      <div className="flex pl-20 justify-center gap-8 mt-8">
+        <div className="w-150 h-120 rounded-lg overflow-hidden transform hover:scale-105 transition-transform">
+                      <div className="w-full h-72">
+                          <img width={500} height={500} src="https://s3-alpha-sig.figma.com/img/3740/8e44/4bdba3a6bef9d68df2d9a06e32e96c61?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=tMn4E3t1mWUW8p0snPi63Vc4B12KOf1PWEbbWPtwYGKC1hsGtxyEW6x7p7Jk4t6eSuCvwk4gEkO-bPuHle4i~EvP2NApS7TUzm~HPsiINohIcFsEZKrBoAZztLmKRb6NpTo7JRLt77kbHJE5og-k-x7q00I3-5HUI4w1QHC2EA5L73nLFmOFXD8IvurM043m3m5E8qt6OoKKVtcylIgXI0LYOOlWOlfoW5ECH3DFxoQJtBgTfH7ezkKzP8K09phBNCvNHcDYwnduXWKbnK4Q7I~l67qXNXCN9vW5ZijPAntRtlQtJ8v~LSFrBGbX2XBCHqlcfYc3A5DkW1cwUN8umQ__" alt="" />
+          </div>
+          <div className="py-15 pr-25  text-lg font-semibold text-gray-700">Dining</div>
+        </div>
+        <div className="w-150 h-120 rounded-lg overflow-hidden transform hover:scale-105 transition-transform">
+                      <div className="w-full h-72">
+                          <img width={500} height={500} src="https://s3-alpha-sig.figma.com/img/b7e3/92a7/f3961ca2b1edab00f7a7640b3c2ed666?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lhYtSir5PCfoPT5svJ1UmcSLk7QcTnynlerSOSzfl4FfvT20TwUnv8GwXECCdhPNztzr2A6n~LAlajr8pMYgjc7f8ePJvPzzgGGYHwEJ95dw5rly~BAa1aMUnqR9z8XYXTo9iLg1E5kx0NitoCHKj1T0VLoWzjkkhLZXMZNVkho3bYJsjfq87uLg6RJk5Wh-JQ3f1vN1zSgnU9c22r3CV9GGIy7dLVa~nY7CtGLYrc~XwMVdOq0oZB~rF63altaAs8hGyuooihYoIH1itGYFlLxYQwJugAi-KLA6oghhz91t5pNy9uOngKEXtfKkcVUH~IjqRvX1GYM19gga-jb3nw__" alt="" />
+          </div>
+          <div className="py-15 pr-25  text-lg font-semibold text-gray-700">Living</div>
+        </div>
+        <div className="w-150 h-120 rounded-lg overflow-hidden transform hover:scale-105 transition-transform">
+                      <div className="w-full h-72">
+                          <img width={500} height={500} src="https://s3-alpha-sig.figma.com/img/77e4/946e/ec6e291e21c9694ce22e6c5b50d777fe?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=oEHXPSTvku5v1Sd81tRVORz09i0PtGOgSAkSkTujWoQye8hfGS9IzAjYFDgvFfdbbA4RNb~s2YX6gXSsRR8UDgPL0eqTA5MoRT0MfPdiefXFATHgWyFIWAoZuaG8m8UkPSdguOROFsoR0B6QMwpyKqM~gU~noctg~0HjCUaAof2qGUy8qqr0~Ev0bqL-AgtGBh~WMSOgtoUbL6NNRRX~oezU2GOsQoRquytVBi0v~Z4z3pD73UJkrGsj2srhdwo0aEkeXTWlX7m8Z-vsWkcrYsZVhIz4TpL3djrD2A45lw4i3c37UVh38PiFkls1kC2qP~ifqvpmcQWUUgn8zIlJrA__" alt="" />
+          </div>
+          <div className="py-15 pr-25  text-lg font-semibold text-gray-700">Bedroom</div>
+        </div>
+      </div>
+    </div>
+
+
+      <section className="pt-1 bg-gray-100 text-center">
+        <h2 className="text-2xl  font-bold text-gray-800">Our Products</h2>
+        <div className="grid grid-cols-4 gap-6 mt-8 w-[1750px] mx-auto text-left">
+          {[
+            { title: "Syltherine", desc: "Stylish cafe chair", price: "Rp 2.500.000", oldPrice: "Rp 3.500.000",  img: p1  },
+            { title: "Leviosa", desc: "Stylish cafe chair", price: "Rp 2.500.000", img: p2 },
+            { title: "Lolito", desc: "Luxury big sofa", price: "Rp 7.000.000", oldPrice: "Rp 14.000.000",  img: p3 },
+            { title: "Respira", desc: "Outdoor bar table and stool", price: "Rp 500.000", img: p4, new: true },
+            { title: "Grifo", desc: "Night lamp", price: "Rp 1.500.000", img: p5 },
+            { title: "Muggo", desc: "Small mug", price: "Rp 150.000", img: p6, new: true },
+            { title: "Pingky", desc: "Cute bed set", price: "Rp 7.000.000", oldPrice: "Rp 14.000.000", img: p7 },
+            { title: "Potty", desc: "Minimalist flower pot", price: "Rp 500.000", img: p8, new: true },
+          ].map((item, index) => (
+            <div key={index} className="bg-white p-4 rounded-lg shadow-md relative transition transform hover:scale-105 hover:shadow-xl">
+             
+             
+              <div className="relative group">
+                <img src={item.img} alt={item.title} className="w-full h-40 object-cover rounded-lg" />
+                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-center items-center transition">
+                  <button className="bg-yellow-500 text-black px-4 py-2 rounded-lg">Add to cart</button>
+                </div>
+              </div>
+              <h3 className="mt-4 text-lg font-semibold text-gray-800">{item.title}</h3>
+              <p className="text-gray-600 text-sm">{item.desc}</p>
+              <p className="text-gray-800 font-bold">{item.price} {item.oldPrice && <span className="text-gray-400 line-through ml-2">{item.oldPrice}</span>}</p>
+            </div>
+          ))}
+        </div>
+        <button className="mt-8 px-6 py-2 border border-gray-800 text-gray-800 rounded-lg hover:bg-gray-800 hover:text-white transition">Show More</button>
+          </section>
+          <section className="py-12 bg-[#FCF8F3] text-center">
+        <div className="max-w-6xl mx-auto grid grid-cols-2 gap-6 items-center">
+          <div className="text-left">
+            <h2 className="text-2xl font-bold text-gray-800">50+ Beautiful rooms inspiration</h2>
+            <p className="text-gray-600 mt-2">
+              Our designer already made a lot of beautiful prototype of rooms that inspire you.
+            </p>
+            <button className="mt-4 bg-yellow-500 text-white py-2 px-4 rounded-lg">
+              Explore More
+            </button>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <img src="https://s3-alpha-sig.figma.com/img/f89a/66d9/4961c3801f4c07439f27b13468e5e545?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=H5KHjTu52ZwQcfjof0YVGFaiK7nqYv2zMcziJa9wiOC0~jVEVgSotiPmDuxgX4eYL04OqH9Sa6cwCDXOcGn5LOsBZLot9krqnMdnVor8CZv~pic8GwR~eectRPXQ~0w8c9SOx3uCtpSd1Eg6VxoisyV935oPWqFXiiEn6HtJTvm6gQWdTaJr3ABsv8AxIVsxrQKghcVVqGeYW-YFSy~Yzzk~J6zI0gXG24J6zZwGvk7caDGmQgFKNtQpuC3BCQiRQwVTT1xHHg5EXuVIUT3h51W9oCds9AdJtn-aO5aR4u~OaHix5tbWr~C-Y-Zyd5VOnXjsOqgsIT7Zv87s4cX2bA__" alt="Room 1" className="rounded-lg shadow-md" />
+            <img src="https://s3-alpha-sig.figma.com/img/acc4/8179/d1b18b523420e79dda1e92951ecde49b?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=NiHqG5YZ3s9dQTv7MkHQlW8-EMfIhnjxc4rrhnBotPJDARiA9wWDxLvyQ7T9wEzbIit~P5b0AkjjTnIvT~m0n7ZKYEpbwQ5EO4034F8AlZHD~1Rol2-Q8KuuU3p-yxLoHLfBKu43LisTe8TE73QFTJPI0PKUl6IQJsvzzVj4jbWITKbJAEfg34TSzbNQ9X4qdosKGrLGoEFXuzh5cpyZHvuoXJ8A4qpBbSbUvHzrVorNUhKRfgaiAN8vTEKF~CZsV~sHEOixVJC6C1FDhtbyuGYQyB1zk6yHdq2IaieAKZqMqbpQVWX5QrJMSW5o9oywHo5krqS7NTq~I462tX1yRg__" alt="Room 2" className="rounded-lg shadow-md" />
+          </div>
+        </div>
+          </section>
+          <section className="py-12 bg-white text-center">
+        <h2 className="text-2xl font-bold text-gray-800">Share your setup with</h2>
+        <h3 className="text-3xl font-bold text-gray-900">#FuniroFurniture</h3>
+        <div className="max-w-6xl mx-auto grid grid-cols-3 gap-4 mt-8">
+          <img src="https://s3-alpha-sig.figma.com/img/a4bb/2814/2f4aae4edaceced4645de9ad49216504?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ZtO1yZlsBnhEr18J3aS5abDk~dkRPHJSi3Rd00j~qb0fGEWeUIh1MM7Wv2S7J7A47V-TqISn~gL9eMOaEDb9X3uYBmHe6er-eMH1aTZCZKMKErTiXUVIP2BQdhSi274HOWaoxNM4U4Vf3cpnaQ-2ehrMsQd4yD-kMrllL4R7pQmEubZAye-bAWMvboGp6yCtEuLIr2iaY75NLNH1Tq2Q28M4LcTQ9Qq3Sv1pX3m5mCnzKlqoHBQJzkUCjBRh7zIzzdrVgPKZ5agMkM2Y0UiFOqgS9U19tCB2ybJXf~~dQOZPi0kfbVAd4H39UGa4X5O-~eluyz7hQgoVsRUIGTZKxw__" alt="Setup 1" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/057c/3bab/f9de6c6ff643cd2e7661215ad26aed46?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FYtAssESzVQGreYUdg1bG7cb3-aLrUFNaLtk7nkpSO591knLLIioeH~GpF6lmQG-WMNdn5yD0M245s64qeiETQnMNUjD-tnQjiRwT7odJa5EZ0kXVx5CgSskjKO8e3IowgH5STVmtIIbWUvokh~psSLoj~CBEKRgl22IV2M~UVDocR6uo40ZEnWnQCLjqC~KEOmtimOHE1iyOUfG8163~DTdZMgtPrYOnQOeRFce1fj6CdiQW7ii1jeADLCh6qIQYNLnse3lNHLXVnL6MyfhzfU~wB9K3LmVCsw7YHyoEmabOtBCfn6KZjfQYCKAx07mfu338XvyqMTe9SzWqmSKyg__" alt="Setup 2" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/394a/75bc/77d8a98d7cff3b010ba29ad5b60844d7?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=aahwkcRqVBtKo3HJiyQDTel6d5tBrv0-UZLh1riOZPw3D4SjUa2DkPnto95QRxeZ2TLymA5a49JEKTdgsaiCQypUHJ1VeLNXhCxjR4EesXjsmYFEWy3CKmjUFGSOLg~vXR37ZILXVftFy9Xlc8JqWAYzcN7fdcn9YFQ6mMERHA~xUxmCdFIqYZ1nYaUB64Azjkp~JDgyPzbsLygkQCtUHxWuTYv5lg8DLpt5ES0wyhgU8MlC-K1d29~yQDSMViy8eqZJyrWeaRI-SdMWnSpjsEj4VT8Ys5nQHZdu6hZ8mzAPfqhvtE-yZCxvzkodlbEpEaf~DWZs3s1v~MqUWZ74bg__" alt="Setup 3" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/394a/75bc/77d8a98d7cff3b010ba29ad5b60844d7?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=aahwkcRqVBtKo3HJiyQDTel6d5tBrv0-UZLh1riOZPw3D4SjUa2DkPnto95QRxeZ2TLymA5a49JEKTdgsaiCQypUHJ1VeLNXhCxjR4EesXjsmYFEWy3CKmjUFGSOLg~vXR37ZILXVftFy9Xlc8JqWAYzcN7fdcn9YFQ6mMERHA~xUxmCdFIqYZ1nYaUB64Azjkp~JDgyPzbsLygkQCtUHxWuTYv5lg8DLpt5ES0wyhgU8MlC-K1d29~yQDSMViy8eqZJyrWeaRI-SdMWnSpjsEj4VT8Ys5nQHZdu6hZ8mzAPfqhvtE-yZCxvzkodlbEpEaf~DWZs3s1v~MqUWZ74bg__" alt="Setup 4" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/6b7d/b8c4/94f946a8fffd305fbd1dafe75395cc6f?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=T6gS73n8J6VqUF2WRTjlt93RxIo01Vgm-XFiZuLV84X4YnqmicB-dPpsTFJ9qncS06MzmVpuQCNhx7t1Ra73sSDAoADVPalAq6YAKC26BIwZSWBTfRKLUwb2oHLoamfv22leXk9ETp2b2VshO6eXm04NgjDRDCnKZK6uz0zSWFsUnmCIFMajOsYIMbLEGY26~S0Rf1JguPWUNheNkerdp-E4WMNa8lG6aWNscxW-y~C11hAQ2xFqU0oIDXiw2327URzNX-adQHTkUCTZCoX8C~xePrTCUssS1o4nbSw1JBFWTy4YAtuyiY9DaJxqntfxz7uOPl3qS6F3izqokVDdZA__" alt="Setup 5" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/7843/84ee/37569bf147f74d2ca44831a4a6ef3ee5?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=kUR3ZW-0YzvOMonKFCTmQWl9UIjjE6Ab5jS50LiQIY3A4ZoFpneL8Fh8JpxeXy1kqJXeTbdglObkiy6Oa0rMEVCnNS6DpMq99cpj2quJwmcJPtiTXFjfvmzMn8gW9fYFumRNlFtitjPIofQKCNmBL4cuXDX8IptBfFPFBjMCvpO-gSEfi8En0A3xr73jIGK4v5zqt5V1-vmazWJYuycPiloI3B180pbGkTwpqXZAU1nqcEG2YhvaD0-IvkUq-vKtEi7gNr96HNFC4P54BgzjJbc78xaxhKLxibMk~aI4vI1Pex0mVsldobPoPL~R7dPNhUnqM6L3rdAXWWiw7tgffQ__" alt="Setup 6" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/9954/4b25/1da1dc5c1c4cd5091a6369784c3b3366?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=U6k7NvBVIcB1na2DytwQ3ecvte7gxN3ae5KBOvHPiU4BXc1Mjxb9ixznjL~U-oriCS24air2YMiYZqmxoJvNRVHY6UbbOfBFTJRxK~bw7tUfhbvu61~cUOYKlFO8iPe5YU-FF6U~-wmV31XvI1oIV86M2awLIYyOWGXLDo1tnp6f0LbmKNzzB-1lgAeWq7GshSUQPyflF4NQ3BXkqtuxBATMsZELSXWC-KoKwxzoYTHoLpmpOndaPnxCn5WH~uSuELa0n-iwpn3h~YkbIUyMmUSzuS3WA45xw4f3iV-WA6qRUvRMKLYc1WRb9mXcU1pZ-Qj7xcpkR-0ZhIXO9PQVeQ__" alt="Setup 7" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/e8ee/1dc9/413c23f2d7371be94a52ea9020db45cc?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=IZ6KqzNmjagUrz0mF5fXoy80JihbUeTsQpYJqgfm99iGBPCTYv9MBJqcZFUoOZ~5pw-f3SRUJK0Peqcq9lU3kAqGMde87T7ZMCwOW86lk99I3wFe07SJhu68yWtL37QxfAQUVcnMb2tFAMEjE~qFAsqy63nsughX1FZENPgxmkKlptTqfAV6WrjxfL7MGUFiW5yiHE641FxzZZjiVIWbK82~eUpINP1floK4k5u09XB~t5VNHh11hbB10WqNQkUdmqMKcP~uTzvpWr~jIqwYOg9EGVGKGxnNykudI98xHQxMl7ePKQ3obh1Cd87n9tjBSm9ZrSqNJhf-pLX2ybU7kA__" alt="Setup 8" className="rounded-lg shadow-md" />
+          <img src="https://s3-alpha-sig.figma.com/img/e8ee/1dc9/413c23f2d7371be94a52ea9020db45cc?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=IZ6KqzNmjagUrz0mF5fXoy80JihbUeTsQpYJqgfm99iGBPCTYv9MBJqcZFUoOZ~5pw-f3SRUJK0Peqcq9lU3kAqGMde87T7ZMCwOW86lk99I3wFe07SJhu68yWtL37QxfAQUVcnMb2tFAMEjE~qFAsqy63nsughX1FZENPgxmkKlptTqfAV6WrjxfL7MGUFiW5yiHE641FxzZZjiVIWbK82~eUpINP1floK4k5u09XB~t5VNHh11hbB10WqNQkUdmqMKcP~uTzvpWr~jIqwYOg9EGVGKGxnNykudI98xHQxMl7ePKQ3obh1Cd87n9tjBSm9ZrSqNJhf-pLX2ybU7kA__" alt="Setup 8" className="rounded-lg shadow-md" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
